e47972695796a75e6845e0d41f42166e9e88db89
Rolf <rolfschellenberger@gmail.com>
2021-12-26 09:39:54 +0100

    Day 1

--- src/main/kotlin/com/rolf/day01/Day01.kt
+++ src/main/kotlin/com/rolf/day01/Day01.kt
@@ -1,6 +1,7 @@
 package com.rolf.day01
 
 import com.rolf.util.readLines
+import com.rolf.util.splitLines
 
 const val DAY = "01"
 
@@ -8,7 +9,7 @@ fun main() {
     println("+--------+")
     println("| Day $DAY |")
     println("+--------+")
-    val lines = readLines("$DAY.txt")
+    val lines = readLines("/$DAY.txt")
 
     println("-- Part 1 --")
     solve1(lines)
@@ -17,7 +18,19 @@ fun main() {
 }
 
 fun solve1(lines: List<String>) {
+    val characters = splitLines(lines, chunkSize = 1).flatten()
+    println(characters.map { if (it == "(") 1 else -1 }.sum())
 }
 
 fun solve2(lines: List<String>) {
+    val characters = splitLines(lines, chunkSize = 1).flatten()
+    var floor = 0
+    for ((index, character) in characters.withIndex()) {
+        if (character == "(") floor++
+        else floor--
+        if (floor < 0) {
+            println(index + 1)
+            return
+        }
+    }
 }
